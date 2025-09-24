78831550089ddaf337ccc9f42085adadb62792f2
Minect86 <kobzeve668@gmail.com>
2025-09-24 16:34:17 +0300

    HSM

--- BetterSpawnTimer/Plugin.cs
+++ BetterSpawnTimer/Plugin.cs
@@ -6,7 +6,7 @@ namespace BetterSpawnTimer
     public class Plugin : Plugin<Config>
     {
         public override Version RequiredExiledVersion => new Version(9, 8, 1);
-        public override Version Version => new Version(1, 0, 1);
+        public override Version Version => new Version(1, 0, 2);
         public override string Author => "minect86";
         public override string Name => "BetterSpawnTimer";
 
